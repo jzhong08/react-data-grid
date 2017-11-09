4d11fab4f9959805e72f0d3d8f211065f0d8984a
Jerry Zhong <JZhong@dkpartners.com>
2017-11-09 15:13:01 -0500

    Merged column re-order feature from draggable-header into corresponding files in draggable directory.

--- packages/react-data-grid-addons/src/draggable/DraggableHeaderCell.js
+++ packages/react-data-grid-addons/src/draggable/DraggableHeaderCell.js
@@ -1,4 +1,4 @@
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
 import React, {PropTypes, Component} from 'react';
 import { _constants, HeaderCell } from 'react-data-grid';
 const { DragItemTypes } = _constants;
@@ -8,7 +8,7 @@ class DraggableHeaderCell extends Component {
   componentDidMount() {
     let connectDragPreview = this.props.connectDragPreview;
     let img = new Image();
-    img.src = './assets/images/drag_column_full.png';
+    img.src = './dist/assets/images/drag_column_full.png';
     img.onload = function() {
       connectDragPreview(img);
     };
@@ -21,25 +21,41 @@ class DraggableHeaderCell extends Component {
   }
 
   render() {
-    const { connectDragSource, isDragging} = this.props;
+    const {
+      connectDragSource,
+      isDragging,
+      connectDropTarget,
+    } = this.props;
+
     if (isDragging) {
       return null;
     }
-    return connectDragSource(<div style={{cursor: 'move'}}><HeaderCell {...this.props}/></div>);
+
+    // width: 0 - move the cloned header to be exactly at where the mouse pointer is
+    return connectDragSource(
+      connectDropTarget(
+        <div style={{ width: 0, cursor: 'move' }}>
+            <HeaderCell {...this.props} />
+        </div>));
   }
 }
 
 DraggableHeaderCell.propTypes = {
   connectDragSource: PropTypes.func.isRequired,
   connectDragPreview: PropTypes.func.isRequired,
-  isDragging: PropTypes.bool.isRequired
+  isDragging: PropTypes.bool.isRequired,
+  connectDropTarget: PropTypes.func.isRequired,
+  isOver: PropTypes.bool,
+  canDrop: PropTypes.bool
 };
 
+// drop source
 function collect(connect, monitor) {
   return {
     connectDragSource: connect.dragSource(),
     isDragging: monitor.isDragging(),
-    connectDragPreview: connect.dragPreview()
+    connectDragPreview: connect.dragPreview(),
+    connectDropTarget: null
   };
 }
 
@@ -47,9 +63,38 @@ const headerCellSource = {
   beginDrag(props) {
     return props.column;
   },
-  endDrag(props) {
-    return props.column;
+  endDrag(props, monitor) {
+      if (monitor.didDrop() && typeof monitor.getDropResult().source != "undefined") {
+          const source = monitor.getDropResult().source;
+          const targetKey = monitor.getDropResult().target;
+          return props.onHeaderDrop(source, targetKey);
+      }
+      return props.column;
   }
 };
 
-export default DragSource(DragItemTypes.Column, headerCellSource, collect)(DraggableHeaderCell);
+// drop target
+const target = {
+    drop(props, monitor) {
+        let source = monitor.getItem().key;
+        let targetKey = props.column.key;
+        return {
+            source: source,
+            target: targetKey
+        };
+    }
+};
+
+function targetCollect(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop(),
+        draggedHeader: monitor.getItem()
+    };
+}
+
+DraggableHeaderCell = DropTarget(DragItemTypes.Column, target, targetCollect)(DraggableHeaderCell);
+DraggableHeaderCell = DragSource(DragItemTypes.Column, headerCellSource, collect)(DraggableHeaderCell);
+
+export default DraggableHeaderCell;
